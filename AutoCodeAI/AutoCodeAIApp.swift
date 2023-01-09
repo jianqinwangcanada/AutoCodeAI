818c7a52d3e380a41383e728a8f035acabd4a8d7
jianqin wang <jianqin.wang01@stclairconnect.ca>
2023-01-09 14:45:28 -0500

    Initial Commit

---
+++ AutoCodeAI/AutoCodeAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoCodeAIApp.swift
+//  AutoCodeAI
+//
+//  Created by Jianqin Wang on 2023-01-09.
+//
+
+import SwiftUI
+
+@main
+struct AutoCodeAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
